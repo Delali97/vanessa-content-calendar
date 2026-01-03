6491285cf6e23fb5ad914c9f3a378f8e42a952f4
Jennifer Kpolu <jennifer.kpolu@yahoo.com>
2026-01-03 13:43:26 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = 'content-calendar-v1';
+const CACHE_NAME = 'content-calendar-v3';
 const urlsToCache = [
-  '/content-calendar/',
-  '/content-calendar/index.html',
-  '/content-calendar/style.css',
-  '/content-calendar/data.js'
+  './',
+  './index.html',
+  './style.css',
+  './data.js'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
